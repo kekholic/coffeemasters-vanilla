1eb445e195bd3e1509376d8c5a0b19af3ee36b6c
Ivan Makhankov <mahankovage@yandex.ru>
2024-08-25 00:34:04 +0500

    add simple router based on  DOM API

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-// const $ = function(args){ return document.querySelector(args);}
-// const $$ = function(args){ return document.querySelectorAll(args);}
-
-
-// HTMLElement.prototype.on = function(a, b, c){ return this.addEventListener(a, b, c); }
-// HTMLElement.prototype.off = function(a, b){ return this.removeEventListener(a, b); }
-// HTMLElement.prototype.$ = function(s){ return this.querySelector(s); }
-// HTMLElement.prototype.$$ = function(s){ return this.querySelectorAll(s); }
 import Store from './services/Store.js'
 import { loadData } from './services/Menu.js'
+import Router from './services/Router.js'
 
-window.app = {}
-app.store = Store
+window.app = {};
+app.store = Store;
+app.router = Router;
 
 window.addEventListener("DOMContentLoaded", async () => {
-    loadData()
+    loadData();
+    app.router.init();
 })
---
+++ services/Router.js
@@ -0,0 +1,54 @@
+// TODO refactor with receiving a collection of routes with a regex as path and component to render
+const Router = {
+    init: () => {
+        document.querySelectorAll("a.navlink").forEach((a)=> {
+            a.addEventListener("click", (event) => {
+                event.preventDefault();
+                const url = event.target.getAttribute("href");
+                Router.go(url)
+            })
+        })
+
+        // for URL changes
+        window.addEventListener("popstate", (event) => {
+            Router.go(event.state.route, false)
+        })
+        // inital URL can be no "/"
+        Router.go(location.pathname);
+    },
+    go: (route, addToHistory = true) => {
+        console.log(`Going to ${route}`);
+
+        if (addToHistory) {
+            history.pushState({ route }, null, route)
+        }
+        let pageElement = null
+        switch(route) {
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Menu";
+                break;
+            case "/order": 
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Your order";
+                break;
+            default: 
+                if (route.startsWith("/product/")) {
+                    pageElement = document.createElement("h1");
+                    pageElement.textContent = "Details";
+                    const paramId = route.substring(route.lastIndexOf("/") + 1);
+                    pageElement.dataset.id = paramId;
+                }
+        }
+
+        if (pageElement) {
+            let cache = document.querySelector("main")
+            cache.innerHTML = '';
+            cache.appendChild(pageElement);
+            window.scrollX = 0;
+            window.scrollY = 0;
+        }
+    }
+}
+
+export default Router;
