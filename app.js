9deb3b23699c4565bd1dc21177c6c05b42d2ffda
Ivan Makhankov <mahankovage@yandex.ru>
2024-08-26 20:18:30 +0500

    add Web Components

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import Store from './services/Store.js'
 import { loadData } from './services/Menu.js'
 import Router from './services/Router.js'
 
+//Link my Web Components
+import { MenuPage } from './components/MenuPage.js';
+import { OrderPage } from './components/OrderPage.js';
+import { DetailsPage } from './components/DetailsPage.js';
+
 window.app = {};
 app.store = Store;
 app.router = Router;
